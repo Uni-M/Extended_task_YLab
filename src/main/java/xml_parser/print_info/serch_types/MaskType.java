45efd4d77a3dde8be5e8f69b520f3da9900f1d77
Julia Melnikova <bukoffka@inbox.ru>
2021-10-31 16:41:35 +0300

    use constants

--- src/main/java/xml_parser/print_info/serch_types/MaskType.java
+++ src/main/java/xml_parser/print_info/serch_types/MaskType.java
@@ -5,11 +5,14 @@ import xml_parser.model.Node;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static xml_parser.AppConfig.XConstant.APOSTROPHE1;
+import static xml_parser.AppConfig.XConstant.APOSTROPHE2;
+
 public class MaskType implements Type{
     @Override
     public void printToConsole(String arg, Node child) {
 
-        String extension = arg.substring(2, arg.length()-1);
+        String extension = arg.substring(arg.indexOf(APOSTROPHE1), arg.indexOf(APOSTROPHE2));
         final Pattern pattern = Pattern.compile(extension, Pattern.DOTALL);
         Matcher matcher = pattern.matcher(child.getName());
 
--- src/main/java/xml_parser/print_info/serch_types/RegularType.java
+++ src/main/java/xml_parser/print_info/serch_types/RegularType.java
@@ -5,10 +5,13 @@ import xml_parser.model.Node;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static xml_parser.AppConfig.XConstant.APOSTROPHE1;
+import static xml_parser.AppConfig.XConstant.APOSTROPHE2;
+
 public class RegularType implements Type{
     @Override
     public void printToConsole(String arg, Node child) {
-        String regex = arg.substring(1, arg.length()-1);
+        String regex = arg.substring(arg.indexOf(APOSTROPHE1)+1, arg.indexOf(APOSTROPHE2));
         final Pattern pattern = Pattern.compile(regex,Pattern.DOTALL);
         Matcher matcher = pattern.matcher(child.getName());
         while (matcher.find()) {
