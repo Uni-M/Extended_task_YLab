13dbfbeb12e0cd884e3c3da68b9ad367bef8ad1e
Julia Melnikova <bukoffka@inbox.ru>
2021-11-03 15:12:06 +0300

    add changes with storeParams and validator

--- src/main/java/xml_parser/AppConfig/argument_parser/ArgumentParser.java
+++ src/main/java/xml_parser/AppConfig/argument_parser/ArgumentParser.java
@@ -3,6 +3,7 @@ package xml_parser.AppConfig.argument_parser;
 import xml_parser.AppConfig.exeptions.*;
 
 import java.io.File;
+import java.util.Arrays;
 
 import static xml_parser.AppConfig.XConstant.*;
 
@@ -19,20 +20,34 @@ public class ArgumentParser {
     }
 
     private void storeParams(String[] args) throws ArgumentException{
-        inputFileName = fileExists(args[1]); // — это получение имени файла + проверка//
-        if (args.length > 2){
-            typeOfFilter += args[2];
-            stringToFilter += args[3];
+
+        for (int i = 0; i < args.length; i++){
+            switch (args[i]) {
+                case KEY_INPUT_FILE -> inputFileName = fileExists(args[i + 1]); // — это получение имени файла + проверка//
+
+                case KEY_MACK, KEY_MACK_REGULAR -> {
+                    typeOfFilter += args[i];
+                    stringToFilter += args[i + 1];
+                }
+            }
         }
     }
 
     private void validator(String[] args) throws ArgumentException {
-        if (!args[0].equals(KEY_INPUT_FILE)) {
-            throw new ArgumentException("not supported key:" + args[0]);
+
+        if (!Arrays.asList(args).contains(KEY_INPUT_FILE)) {
+            throw new ArgumentException("not supported key");
         }
-        if (args.length > 2 && args[2].equals(KEY_MACK_REGULAR) && args[2].equals(KEY_MACK)){
-            throw new ArgumentException("not supported key:" + args[2]);
+        if (args.length > 2 && !Arrays.asList(args).contains(KEY_MACK_REGULAR ) && !Arrays.asList(args).contains(KEY_MACK)){
+            throw new ArgumentException("not supported key");
         }
+
+//        if (!args[0].equals(KEY_INPUT_FILE)) {
+//            throw new ArgumentException("not supported key:" + args[0]);
+//        }
+//        if (args.length > 2 && args[2].equals(KEY_MACK_REGULAR) && args[2].equals(KEY_MACK)){
+//            throw new ArgumentException("not supported key:" + args[2]);
+//        }
     }
 
     
