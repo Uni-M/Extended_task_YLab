6d1bbba6781e19230e22e7a34c8cba06b9887002
Julia Melnikova <bukoffka@inbox.ru>
2021-11-03 14:48:59 +0300

    add changes with mask type

--- src/main/java/xml_parser/print_info/serch_types/filter_type_class/MaskType.java
+++ src/main/java/xml_parser/print_info/serch_types/filter_type_class/MaskType.java
@@ -7,18 +7,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static xml_parser.AppConfig.XConstant.APOSTROPHE1;
-import static xml_parser.AppConfig.XConstant.APOSTROPHE2;
 
 public class MaskType implements Type {
+
     @Override
     public void printToConsole(String arg, Node child) {
 
-        String extension = arg.substring(arg.indexOf(APOSTROPHE1), arg.indexOf(APOSTROPHE2));
+        String extension = findExtension(arg);
         final Pattern pattern = Pattern.compile(extension, Pattern.DOTALL);
         Matcher matcher = pattern.matcher(child.getName());
 
         if (matcher.find()) {
             System.out.println(child.getName());
         }
+    }
+
+
+    private String findExtension (String arg){
+
+        String argWithoutApostrophe = arg.replaceAll("^'+|'+(?!\\S)", "");
+
+        String substring1 = argWithoutApostrophe.substring(argWithoutApostrophe.indexOf(APOSTROPHE1)+1, argWithoutApostrophe.indexOf("*"));
+        String substring2 = argWithoutApostrophe.substring(argWithoutApostrophe.indexOf("*")+1, argWithoutApostrophe.length()-1);
+
+        return substring1 + ".*?[ 0-9 a-f A-F ]{0,260}+" + substring2;
     }
 }
