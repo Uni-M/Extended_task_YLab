cf901758bd18feafb9f9ee7d34d4498ad2e1d989
Julia Melnikova <bukoffka@inbox.ru>
2021-11-16 01:03:20 +0300

    fix bug

--- src/main/java/parser/NodeParser.java
+++ src/main/java/parser/NodeParser.java
@@ -15,20 +15,13 @@ public class NodeParser extends DefaultHandler {
 
     private String currentValue = "";
     private boolean isFile = false; //XML attribute (~~ is-file="true")
-    private boolean isFolder = false;
     private Comparator filter;    //filter for printing
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) {
-
         if (qName.equals(INCLUDE_NODE)) {
             if (attributes.getValue(IS_FILE).equals(TRUE)){
-                isFolder = false;
                 isFile = true;
-            }
-        }else if (qName.equals(ACTIVE_NODE)){
-            if (!isFile) {
-                isFolder = true;
             }
         }
 
@@ -36,30 +29,25 @@ public class NodeParser extends DefaultHandler {
 
     @Override
     public void endElement(String uri, String localName, String qName) {
-        if(qName.equals(FOLDER_NODE)) {
-            if (folders.size() > 0) {
-                folders.remove(folders.size() - 1);
-            }
+        switch (qName){
+            case ACTIVE_NODE:
+                if (!isFile) {
+                    addFolderName();
+                }
+                break;
+            case INCLUDE_NODE:
+                if (isFile){
+                    checkByFilter();
+                }
+                break;
+            case FOLDER_NODE:
+                if (folders.size() > 0){
+                    folders.remove(folders.size()-1);
+                }
+                break;
+            default:
+                if(!qName.equals("node")) throw new IllegalStateException("Unexpected qName: " + qName);
         }
-//        switch (qName){
-//            case ACTIVE_NODE:
-//                if (!isFile) {
-//                    addFolderName();
-//                }
-//                break;
-//            case INCLUDE_NODE:
-//                if (isFile){
-//                    checkByFilter();
-//                }
-//                break;
-//            case FOLDER_NODE:
-//                if (folders.size() > 0){
-//                    folders.remove(folders.size()-1);
-//                }
-//                break;
-//            default:
-//                if(!qName.equals("node")) throw new IllegalStateException("Unexpected qName: " + qName);
-//        }
     }
 
 
@@ -92,22 +80,5 @@ public class NodeParser extends DefaultHandler {
     @Override
     public void characters(char[] ch, int start, int length) {
         currentValue += new String(ch, start, length).trim();
-
-        if (isFile == true){
-            checkByFilter();
-        }else if (isFolder == true){
-            addFolderName();
-        }
-
-        //            case ACTIVE_NODE:
-//                if (!isFile) {
-//                    addFolderName();
-//                }
-//                break;
-//            case INCLUDE_NODE:
-//                if (isFile){
-//                    checkByFilter();
-//                }
-//                break;
     }
 }
